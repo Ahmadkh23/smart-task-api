47006cc70997ccb46d43eeb516a8d3dcdc4366af
Ahmad Khawalid <50292140+Ahmadkh23@users.noreply.github.com>
2026-05-22 15:12:22 +0300

    add CRUD endpoints , filtering , searching and ownership security

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter , OrderingFilter
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    filterset_fields = ['completed' , 'priority']
+    search_fields = ['title' , 'description']
+    ordering_fields = ['due_date' , 'created_at']
+
+    def get_queryset(self):
+        return Task.objects.filter(owner=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+        
